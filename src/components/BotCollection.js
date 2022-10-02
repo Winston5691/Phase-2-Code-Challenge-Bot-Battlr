83edbcc65c5ed033abfb1ac2565d36274a27a291
Winston Langat <winston.langat@student.moringaschool.com>
2022-10-02 23:12:31 +0300

    Commit

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,15 +1,29 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({bots,handleDelete,listBots}) {
   // Your code here
+  const displayBotCards = bots.map((bot)=>{
+    return(
+       <BotCard 
+     key={bot.id}
+      bot={bot}
+      handleDelete={handleDelete}
+      listBots={listBots}
+      />
+    )    
+  })
+     
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
+        {displayBotCards}
         Collection of all bots
+
       </div>
     </div>
-  );
-}
-
+  )
+  }
 export default BotCollection;
